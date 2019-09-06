f847c385f365f266958224429206baaa7a745bbe
Abhishekdm <abhishekd301@gmail.com>
2019-09-06 21:12:31 +0530

    added server.js file

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
+// importing express
 const express = require("express");
+// importing db-connection
+const connectDb = require("./config/db");
 
+// creating an instance of express
 const app = express();
+// connect to Database
+connectDb();
 
+// providing a response when a request is made on the server
 app.get("/", (req, res) => res.send("API Running"));
 
 const PORT = process.env.PORT || 5000;
 
+// creating a serve on the port 5000
 app.listen(PORT, () => console.log(`server started on port ${PORT} `));
