b90614e8728a550860eaa0955b574f908ec96a77
Abhishekdm <abhishekd301@gmail.com>
2019-09-14 20:47:03 +0530

    backend completed

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,4 +1,8 @@
 const express = require("express");
+// importing request
+const request = require("request");
+// importing config
+const config = require("config");
 // creating a router function
 const router = express.Router();
 // get middleware
@@ -317,6 +321,33 @@ router.delete("/education/:edu_id", auth, async (req, res) => {
   res.json(profile);
 
   await profile.save();
+});
+// @route GET api/profile/github/:username
+// @desc  get user repos from Github
+// @access Public
+router.get("/github/:username", (req, res) => {
+  try {
+    const options = {
+      uri: `https://api.github.com/users/${
+        req.params.username
+      }/repos?per_page=5&sort=created:asc&client_id=${config.get(
+        "githubClientId"
+      )}&client_secret=${config.get("githubSecret")}`,
+      method: "GET",
+      headers: { "user-agent": "node.js" }
+    };
+    request(options, (error, response, body) => {
+      if (error) console.error(error);
+
+      if (response.statusCode !== 200) {
+        return res.status(400).json({ msg: "No Github profile found" });
+      }
+      res.json(JSON.parse(body));
+    });
+  } catch (err) {
+    console.error(err.message);
+    res.status();
+  }
 });
 
 module.exports = router;
