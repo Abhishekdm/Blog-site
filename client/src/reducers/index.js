0b43af1a59e590ef0108429c72467a8d46dba9d4
Abhishekdm <abhishekd301@gmail.com>
2019-09-23 17:26:17 +0530

    edit profile completed

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -3,10 +3,12 @@
 import { combineReducers } from "redux";
 import alert from "./alert";
 import auth from "./auth";
+import profile from "./profile";
 
 // all the reducers that we create will be
 // added to the object
 export default combineReducers({
   alert,
-  auth
+  auth,
+  profile
 });
