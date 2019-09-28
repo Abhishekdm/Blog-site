2f73c84987e018d79f3850c30add29fab2945ba3
Abhishekdm <abhishekd301@gmail.com>
2019-09-28 21:32:07 +0530

    completed

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -4,11 +4,13 @@ import { combineReducers } from "redux";
 import alert from "./alert";
 import auth from "./auth";
 import profile from "./profile";
+import post from "./post";
 
 // all the reducers that we create will be
 // added to the object
 export default combineReducers({
   alert,
   auth,
-  profile
+  profile,
+  post
 });
